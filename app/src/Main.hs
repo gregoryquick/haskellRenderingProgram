9478d2ee29dfef791fb1073861a49324d0244851
gregoryquick <gregory@quicks.me>
2019-10-14 14:42:58 -0400

    Barebones image creation and run command rewrite
    -Created and output png of image as basic testing
    -Changed run command to not just run a docker image from dockerhub but instead build and run localy

--- app/src/Main.hs
+++ app/src/Main.hs
@@ -1,21 +1,21 @@
 module Main where
 
 import Graphics.UI.Gtk
---import Graphics.Rendering.Cairo
+import Graphics.Rendering.Cairo
 
 import Rendering
 
---main = do
---  surface <- createImageSurface FormatARGB32 500 500
---  renderWith surface sketch
---  surfaceWriteToPNG surface "out.png"
 main = do
-    initGUI
-    window <- windowNew
-    button <- buttonNew
-    set window [ containerBorderWidth := 10, containerChild := button ]
-    set button [ buttonLabel := "Hello World" ]
-    onClicked button (putStrLn "Hello World")
-    onDestroy window mainQuit
-    widgetShowAll window
-    mainGUI
+    surface <- createImageSurface FormatARGB32 500 500
+    renderWith surface sketch
+    surfaceWriteToPNG surface "/data/out.png"
+    getLine
+--    initGUI
+--    window <- windowNew
+--    button <- buttonNew
+--    set window [ containerBorderWidth := 10, containerChild := button ]
+--    set button [ buttonLabel := "Hello World" ]
+--    onClicked button (putStrLn "Hello World")
+--    onDestroy window mainQuit
+--    widgetShowAll window
+--    mainGUI
--- run.command
+++ run.command
@@ -1,3 +1,6 @@
+mydir="$(dirname "$BASH_SOURCE")"
+cd $mydir
+docker build -t gregoryquick/project0:v0.1.2 .
 ip=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
 xhost + $ip
-docker run -t -i -d -e DISPLAY=$ip:0 -v /tmp/.X11-unix:/tmp/.X11-unix gregoryquick/project0:v0.1.1
+docker run -t -i -e DISPLAY=$ip:0 -v /tmp/.X11-unix:/tmp/.X11-unix gregoryquick/project0:v0.1.2
